1eb8be8a05e19059a90b24e5987d13b979e6228d
arun <arun.bme2@gmail.com>
2022-08-18 22:36:07 +0530

    Added Login page

--- freightkare_react/src/App.js
+++ freightkare_react/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Authentication/login';
+import Register from './Authentication/register';
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> andsdf fsad
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // const [state,setState] = useState(false)
+
+
+    return (
+        <>
+        
+        <div style={{ position: 'absolute', width: '100%'}}>
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                width: '30%',
+                flexDirection: 'column',
+                margin: 'auto',
+                marginTop: '150px',
+                boxShadow: '0px 0px 20px 13px #f7f7f7',
+                background: 'azure',
+                borderRadius: '20px'
+            }}>
+                <h1>FreightKare</h1>
+                <Login />
+                {/* <Register /> */}
+            </div >
+        </div >
+        </>
+    );
 }
 
 export default App;
